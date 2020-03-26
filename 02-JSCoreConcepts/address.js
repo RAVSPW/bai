628ca86fcfadf21c00b2f32cde503ef2d94b4dba
RAVSPW <34798524+RAVSPW@users.noreply.github.com>
2020-03-26 20:18:16 +0100

    add adres

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -13,3 +13,13 @@ ul. Długa 15
 30-781 Kraków
 POLAND
 */
+const withoutTemplateStrings = title + " " + name + " " + surname + "\n" + "ul. " + street + "\n" + zip + " " + city + "\n" + country.toUpperCase();
+console.log(withoutTemplateStrings);
+
+const withTemplateStrings = `
+${title} ${name} ${surname}
+ul. ${street}
+${zip} ${city}
+${country.toUpperCase()}
+`;
+console.log(withTemplateStrings);
