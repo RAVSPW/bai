ac2383c3144c510ecb1a6360e45e7b49fe44392c
RAVSPW <34798524+RAVSPW@users.noreply.github.com>
2020-03-26 20:36:13 +0100

    Update university.js

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -2,23 +2,23 @@ names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names;
     }
 
     startWith(letter){
-        return // names (array) which starts with the letter
+        return this.names.filter(name => name.charAt(0) === letter);
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        return this.names.sort();
     }
 
     get() {
-        return // names (array) in natural order
+        return this.names;
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+        return this.names.slice(0, n);
     }
 }
 
@@ -27,9 +27,41 @@ console.log(students.get());
 console.log(students.sort());
 console.log(students.startWith('P'));
 console.log(students.startWith('M').sort());
+
 console.log(students.sort().getFirst(4));
+
 console.log(students.startWith('M').sort().getFirst(2));
+
 console.log(students.get());
 
 
+class StudentsExample {
+    constructor(names) {
+        this.names = names;
+    }
 
+    startWith(letter){
+        return [...this.names].filter(name => name.charAt(0) === letter);
+    }
+
+    sort() {
+        return [...this.names].sort();
+    }
+
+    get() {
+        return this.names;
+    }
+
+    getFirst(n) {
+        return [...this.names].slice(0, n);
+    }
+}
+
+names2 = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
+
+studentsExample = new StudentsExample(names2);
+console.log(studentsExample.get());
+console.log(studentsExample.sort());
+console.log(studentsExample.startWith('P'));
+console.log(studentsExample.startWith('M').sort());
+console.log(studentsExample.get());
