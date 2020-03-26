cc2de93ea33a0556958326aec84d2aad20da8618
RAVSPW <34798524+RAVSPW@users.noreply.github.com>
2020-03-26 20:25:10 +0100

    add solutions

---
+++ 02-JSCoreConcepts/loopwhile.js
@@ -0,0 +1,10 @@
+const poczatek = 2;
+const difference = 4;
+const koniec = 14;
+
+let currentValue = poczatek;
+
+while (currentValue <= koniec) {
+    console.log(currentValue);
+    currentValue += difference;
+}
